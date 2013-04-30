336214bb1df1914efa0cea5d07c8c35544bb5c70
unknown <binluo1@gmail.com>
2013-04-29 23:11:41 -0400

    facebook

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import facebook
 from flask import Flask, render_template, send_from_directory
 
 #----------------------------------------
@@ -16,12 +17,32 @@ app.config.update(
 #----------------------------------------
 
 @app.errorhandler(404)
+def printtest():
+    token = 'BAACEdEose0cBAPP3MdyQ3LYNvv4spBtnwxXxu9y9HRZAvmX8rzPquvD2oZBba8cMYKB753ZA4ON4zsJFKlY1ZCwKbzMZCe3Q5FwM3TSyWbZASqw4ZAChhGLKUDfDIbYZCce3acEU39OsfZAp0gUdzox53eleBgYbOuyvc16NuVGNjRHK6oWwQ7hbwqd3LGISAzQhkqYKQBz3BDGWjO0eFwokdKyufQZCjUsXIZD'
+
+    graph = facebook.GraphAPI(token)
+    profile = graph.get_object("me")
+    #friends = graph.get_connections("me", "friends")
+    
+    #friend_list = [friend['name'] for friend in friends['data']]
+    
+    #print friend_list
+    
+    g_profile = graph.get_object("grainger")
+    num_likes = g_profile['likes']
+    
+    return num_likes
+
 def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title=printtest())
+
+@app.route("/homepage")
+def homepage():
+    return render_template('homepage.html')
 
 #----------------------------------------
 # launch
